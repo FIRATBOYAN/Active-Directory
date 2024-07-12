a4aa9440e15fd67f9f04a3bee831eb3bada50c2e
Fırat Boyan <53865744+FIRATBOYAN@users.noreply.github.com>
2024-07-12 11:07:43 +0300

    Update GET-SearchGroupMembers.ps1

--- GET-SearchGroupMembers.ps1
+++ GET-SearchGroupMembers.ps1
@@ -1,14 +1,50 @@
-﻿function SearchGroup {
+$TelifHaklariMetni = @"
+##############################################
+#
+# Bu Script'in yazarı, Fırat Boyan'dır ve sadece amacına uygun olarak kullanılabilir.
+# Bu Script, Fırat Boyan'ın izni olmadan hiçbir makalede referans gösterilmeden yayınlanamaz.
+#
+# www.firatboyan.com
+#
+##############################################
+"@
+
+Write-Output $TelifHaklariMetni
+
+do {
+    Write-Host "Onaylıyor musunuz? (Evet/Hayır): " -ForegroundColor Cyan -NoNewline
+    $Onay = Read-Host
+    if ($Onay -eq "Evet") {
+
+        Write-Output ""
+        
+        Write-Host "Script çalıştırılıyor..." -ForegroundColor Yellow
+        
+        Start-Sleep -Seconds 3
+        
+    } elseif ($Onay -eq "Hayır") {
+
+        Write-Output ""
+        
+        Write-Host "Onay verilmedi. Script'ten çıkış yapılıyor..." -ForegroundColor Yellow
+        
+        Start-Sleep -Seconds 3
+        
+        exit
+    } else {
+        Write-Output "Geçersiz giriş. Lütfen 'Evet' veya 'Hayır' yazınız."
+    }
+} until ($Onay -eq "Evet" -or $Onay -eq "Hayır")
+
+function SearchGroup {
     while ($true) {
-        # Komut satırından 2-3 satır boşluk bırak.
+
         Write-Host ""
         Write-Host ""
 
-        # Kullanıcıdan anahtar kelime al.
         Write-Host -NoNewline "Lütfen grup anahtar kelimesini girin: " -ForegroundColor Cyan
         $keyword = Read-Host
 
-        # Anahtar kelimeye göre grupları ara
         $groups = @(Get-ADGroup -Filter "Name -like '*$keyword*'")
 
         if ($groups.Count -eq 0) {
@@ -16,7 +52,6 @@
             continue
         }
 
-        # Grupları numaralandırarak listele.
         Write-Host ""
         Write-Host "Anahtar kelimeye uygun gruplar:" -ForegroundColor Yellow
         $index = 1
@@ -25,11 +60,10 @@
             $index++
         }
 
-        # Geçerli bir grup numarası girilene kadar döngü.
         $validSelection = $false
         $groupNumber = 0
         while (-not $validSelection) {
-            # Kullanıcıdan grup numarasını al
+
             Write-Host -NoNewline "Hangi grubu seçmek istiyorsunuz? (numara girin): " -ForegroundColor Cyan
             $input = Read-Host
             if ([int]::TryParse($input, [ref]$groupNumber) -and $groupNumber -ge 1 -and $groupNumber -le $groups.Count) {
@@ -39,11 +73,9 @@
             }
         }
 
-        # Seçilen grubu belirle.
         $selectedGroup = $groups[$groupNumber - 1]
         $groupDN = $selectedGroup.DistinguishedName
 
-        # Grup üyelerini listele ve üye sayısını belirle.
         $groupMembers = Get-ADGroupMember -Identity $groupDN
         $memberCount = @($groupMembers).Count
 
@@ -67,7 +99,6 @@
         Write-Host ""
         Write-Host ("Grup üyeleri listesi tamamlandı. Grup üye sayısı: {0}" -f $memberCount) -ForegroundColor Green
 
-        # Başka bir grup için arama yapma isteğini sor
         $responseValid = $false
         while (-not $responseValid) {
             Write-Host -NoNewline "Başka bir grup için arama yapmak ister misiniz? (Evet/Hayır): " -ForegroundColor Cyan
@@ -97,5 +128,4 @@
     }
 }
 
-# İlk arama fonksiyonunu çağır.
 SearchGroup
