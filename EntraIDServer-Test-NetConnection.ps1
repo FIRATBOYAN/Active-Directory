f32543d4a366f9e871f74e29b0cab71a7f82e421
Fırat Boyan <53865744+FIRATBOYAN@users.noreply.github.com>
2025-01-13 23:15:39 +0300

    Add files via upload

---
+++ EntraIDServer-Test-NetConnection.ps1
@@ -0,0 +1,18 @@
+﻿
+$DomainControllers = @{
+    "SRVDC01.domain.local" = @(53,88,464,3268,3269,389,636,135,137,138,139,445,123,49443)
+    "SRVDC02.domain.local" = @(53,88,464,3268,3269,389,636,135,137,138,139,445,123,49443)
+}
+
+foreach ($DC in $DomainControllers.Keys) {
+
+    foreach ($Port in $DomainControllers[$DC]) {
+        $check = Test-NetConnection -ComputerName $DC -Port $Port -WarningAction SilentlyContinue
+        if ($check.TcpTestSucceeded) {
+            Write-Host ($DC + " on port " + $Port + ": Connection Successful") -ForegroundColor Green
+        } else {
+            Write-Host ($DC + " on port " + $Port + ": Connection Failed") -ForegroundColor Red
+        }
+    }
+}
+
